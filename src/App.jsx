deee7bd964a026f88a50ee1b5d5c7f985fcc1b2b
xordox <coolprabin2000@gmail.com>
2024-08-06 18:14:17 +0545

    useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import NotFound from "./pages/NotFound";
+import { useRef } from "react";
+
 
 const App = () => {
+
+  // useRef is used in alternate to js document.getElementById method
+  //not suitable for referencing multiple elements at once
+let greet = useRef();
+let testGreet = useRef();
+
+const handleGreet = ()=>{
+  greet.current.innerText = "Welcome";
+}
+
+const handleGreet2= ()=> {
+  greet.current.innerHTML = "<ul><li>A</li><li>B</li></ul>" //current returns first reference
+}
+
+
+const handleTestGreet = () => {
+  testGreet.innerText = "woola";
+}
   return (
     <div>
-      <BrowserRouter> {/*alternate <HashRouter/> */}
-      <Routes>
-        <Route path = "/" element= {<HomePage/>}/>
-        <Route path = "/about/:id/:name" element = {<AboutPage/>}/>
-        <Route path = "/contact" element = {<ContactPage/>}/>
-        <Route path = "*" element = {<NotFound/>}/>
-      </Routes>
-      </BrowserRouter>
+      <h1 ref ={greet}>Hello there</h1>
+      <button onClick={handleGreet}>CLick</button>
+      <button onClick={handleGreet2}>CLick Me</button>
+      
+      <h2 ref ={(h2)=>testGreet=h2}>Hello there</h2>
+      <h2 ref ={(abc)=>testGreet=abc}>Hello there</h2>
+      <button onClick={handleTestGreet}>CLick Me</button>
+      <h2 ref ={(abc)=>testGreet=abc}>HI there</h2>
     </div>
   );
 };
